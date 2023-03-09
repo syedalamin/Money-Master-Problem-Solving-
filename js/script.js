104d466bdaa3483af7c446e86d7c1e1a7b2d84fe
SyedAlamin <syedalamin2580@gmail.com>
2023-03-09 19:57:46 +0600

    error solved don

--- js/script.js
+++ js/script.js
@@ -37,11 +37,12 @@ const calculateExpense = () => {
 const calculateSavings = () => {
   // calculate saving amount
   const savePercentage = document.getElementById("save").value;
+  const income = document.querySelector("#income").value;
 //   Validate saving percentage value
   if (savePercentage < 0) {
     alert("Provide positive saving value");
   }
-  const savingAmount = (savePercentage / 100) + income;
+  const savingAmount = (savePercentage / 100) * income;
 
   // calculate remaining balance
   const balance = document.getElementById("balance").innerText;
